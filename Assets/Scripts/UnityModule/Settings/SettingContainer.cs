e344ee454d79131529e619ddaf4ec258ff8acdfc
Tetsuya Mori <monry84@gmail.com>
2018-07-05 21:36:16 +0900

    :sparkles: Split envinronment and default

--- Assets/Scripts/UnityModule/Settings/SettingContainer.cs
+++ Assets/Scripts/UnityModule/Settings/SettingContainer.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
+
 #endif
 
 namespace UnityModule.Settings
@@ -24,22 +25,42 @@ namespace UnityModule.Settings
 
     public class SettingContainer : ScriptableObject, ISettingContainer
     {
-        private const string Path = "SettingContainer";
+        private const string PrefixDefault = "Default";
+
+        private const string PrefixEnvironment = "Environment";
+
+        private const string PathFormat = "{0}SettingContainer";
 
         private const string Extension = ".asset";
 
         private static ISettingContainer instance;
 
-        // XXX: Should support to Dependency Injection...
-        public static ISettingContainer Instance =>
-            instance
-                // Try to load from Resources
-                ?? (instance = Resources.Load<SettingContainer>(Path))
+        private static Dictionary<string, ISettingContainer> InstanceMap { get; } = new Dictionary<string, ISettingContainer>();
+
+        public static ISettingContainer ResolveContainerInstance<TSetting>()
+        {
+            if (typeof(IEnvironmentSetting).IsAssignableFrom(typeof(TSetting)))
+            {
+                return GetInstance(PrefixEnvironment);
+            }
+
+            return GetInstance(PrefixDefault);
+        }
+
+        private static ISettingContainer GetInstance(string prefix)
+        {
+            if (!InstanceMap.ContainsKey(prefix))
+            {
+                InstanceMap[prefix] =
+                    Resources.Load<SettingContainer>(string.Format(PathFormat, prefix))
 #if UNITY_EDITOR
-                // Create empty instance
-                ?? (instance = CreateAsset())
+                    ?? CreateScriptableObject(prefix)
 #endif
-                ;
+                    ;
+            }
+
+            return InstanceMap[prefix];
+        }
 
         [SerializeField] private List<Setting> settingList = new List<Setting>();
 
@@ -52,7 +73,7 @@ namespace UnityModule.Settings
 
         public TSetting Get<TSetting>() where TSetting : ISetting
         {
-            return (TSetting)SettingList?.FirstOrDefault(x => x is TSetting);
+            return (TSetting) SettingList?.FirstOrDefault(x => x is TSetting);
         }
 
         public IEnumerable<TSetting> GetAll<TSetting>() where TSetting : ISetting
@@ -68,6 +89,9 @@ namespace UnityModule.Settings
         public void Add<TSetting>(TSetting setting) where TSetting : ISetting
         {
             settingList.Add(setting as Setting);
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+#endif
         }
 
 #if UNITY_EDITOR
@@ -77,21 +101,27 @@ namespace UnityModule.Settings
         /// [CreateAssetMenu] を用いても良かったが、パスを固定にしたかったので、敢えてヤヤコシイ処理を入れている
         /// </summary>
         [MenuItem("Assets/Create/SettingContainer")]
-        public static SettingContainer CreateAsset()
+        public static void CreateScriptableObjects()
         {
-            if (File.Exists(System.IO.Path.Combine(Application.dataPath, "Resources", $"{Path}{Extension}")))
+            CreateScriptableObject(PrefixDefault);
+            CreateScriptableObject(PrefixEnvironment);
+        }
+
+        private static SettingContainer CreateScriptableObject(string prefix)
+        {
+            if (File.Exists(Path.Combine(Application.dataPath, "Resources", $"{string.Format(PathFormat, prefix)}{Extension}")))
             {
-                return Resources.Load<SettingContainer>(Path);
+                return Resources.Load<SettingContainer>(PathFormat);
             }
 
-            var directoryPath = System.IO.Path.Combine(Application.dataPath, "Resources");
+            var directoryPath = Path.Combine(Application.dataPath, "Resources");
             if (!string.IsNullOrEmpty(directoryPath) && !Directory.Exists(directoryPath))
             {
                 Directory.CreateDirectory(directoryPath);
             }
 
             var settingContainer = CreateInstance<SettingContainer>();
-            AssetDatabase.CreateAsset(settingContainer, System.IO.Path.Combine("Assets", "Resources", $"{Path}{Extension}"));
+            AssetDatabase.CreateAsset(settingContainer, Path.Combine("Assets", "Resources", $"{string.Format(PathFormat, prefix)}{Extension}"));
             AssetDatabase.Refresh();
             return settingContainer;
         }

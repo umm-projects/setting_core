8c0a196174c3122c1493c6d06e3513bcbcea5ad1
Tetsuya Mori <monry84@gmail.com>
2018-08-31 12:53:54 +0900

    :shirt: Re-format

--- Assets/Scripts/UnityModule/Settings/Setting.cs
+++ Assets/Scripts/UnityModule/Settings/Setting.cs
@@ -1,12 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using JetBrains.Annotations;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using UnityEngine;
 
-namespace UnityModule.Settings {
-
+namespace UnityModule.Settings
+{
     public interface ISetting
     {
     }
@@ -35,16 +36,19 @@ namespace UnityModule.Settings {
 
         private const string EnvironmentDirectoryName = "Environment";
 
-        protected static TSetting CreateAsset() {
+        protected static TSetting CreateAsset()
+        {
             var projectSetting = CreateInstance<TSetting>();
             var assetDirectoryPath = Path.Combine(
                 BaseDirectoryName,
                 typeof(IEnvironmentSetting).IsAssignableFrom(typeof(TSetting)) ? EnvironmentDirectoryName : string.Empty
             );
             var assetName = typeof(TSetting).Name;
-            if (!string.IsNullOrEmpty(Path.Combine(Application.dataPath, assetDirectoryPath)) && !Directory.Exists(Path.Combine(Application.dataPath, assetDirectoryPath))) {
+            if (!string.IsNullOrEmpty(Path.Combine(Application.dataPath, assetDirectoryPath)) && !Directory.Exists(Path.Combine(Application.dataPath, assetDirectoryPath)))
+            {
                 Directory.CreateDirectory(Path.Combine(Application.dataPath, assetDirectoryPath));
             }
+
             AssetDatabase.CreateAsset(projectSetting, AssetDatabase.GenerateUniqueAssetPath(Path.Combine("Assets", assetDirectoryPath, $"{assetName}.asset")));
             AssetDatabase.Refresh();
             SettingContainer.ResolveContainerInstance<TSetting>().Add(projectSetting);
